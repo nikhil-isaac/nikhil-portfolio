e8ab743463c1dd4f46897011074e725dc153eb4c
nikhil-isaac <37073895+nikhil-isaac@users.noreply.github.com>
2024-01-10 18:22:56 +0530

    convert into functional component - HeaderThree

--- src/component/header/HeaderThree.jsx
+++ src/component/header/HeaderThree.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { FaFacebookF, FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { FiX, FiMenu } from "react-icons/fi";
 import Scrollspy from "react-scrollspy";
@@ -12,28 +12,10 @@ const SocialShare = [
   },
   { Social: <FaGithub />, link: "https://github.com/nikhil-isaac" },
 ];
-class HeaderThree extends Component {
-  constructor(props) {
-    super(props);
-    this.menuTrigger = this.menuTrigger.bind(this);
-    this.CLoseMenuTrigger = this.CLoseMenuTrigger.bind(this);
-    this.stickyHeader = this.stickyHeader.bind(this);
 
-    //  this.subMetuTrigger = this.subMetuTrigger.bind(this);
-    window.addEventListener("load", function () {});
-  }
-  menuTrigger() {
-    document.querySelector(".header-wrapper").classList.toggle("menu-open");
-  }
-
-  CLoseMenuTrigger() {
-    document.querySelector(".header-wrapper").classList.remove("menu-open");
-  }
-
-  stickyHeader() {}
-
-  render() {
-    window.addEventListener("scroll", function () {
+const HeaderThree = (props) => {
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
       var value = window.scrollY;
       if (value > 100) {
         document.querySelector(".header--fixed").classList.add("sticky");
@@ -53,91 +35,100 @@ class HeaderThree extends Component {
         };
       }
     }
-    const { color = "default-color" } = this.props;
-    let logoUrl = (
-      <img
-        style={{ width: "30px", opacity: 0.8 }}
-        src={logo}
-        alt="Nikhil Manohar"
-      />
-    );
+  }, []);
 
-    return (
-      <header className={`header-area header-style-two header--fixed ${color}`}>
-        <div className="header-wrapper">
-          <div className="header-left d-flex align-items-center">
-            <div className="logo">
-              <a href={this.props.homeLink}>{logoUrl}</a>
-            </div>
-            <nav className="mainmenunav d-lg-block ml--50">
-              <Scrollspy
-                className="mainmenu"
-                items={[
-                  "home",
-                  "about",
-                  "skills",
-                  "portfolio",
-                  "blog",
-                  "contact",
-                ]}
-                currentClassName="is-current"
-                offset={-200}
-              >
-                <li>
-                  <a href="#home">Home</a>
-                </li>
-                <li>
-                  <a href="#about">About</a>
-                </li>
-                <li>
-                  <a href="#skills">Skills</a>
-                </li>
-                <li>
-                  <a href="#portfolio">Portfolio</a>
-                </li>
-                {/* <li>
-                  <a href="#blog">Blog</a>
-                </li> */}
-                <li>
-                  <a href="#contact">Contact</a>
-                </li>
-              </Scrollspy>
-            </nav>
+  // Define functions directly, no need to bind them
+  const menuTrigger = () => {
+    document.querySelector(".header-wrapper").classList.toggle("menu-open");
+  };
+
+  const closeMenuTrigger = () => {
+    document.querySelector(".header-wrapper").classList.remove("menu-open");
+  };
+
+  const { color = "default-color", homeLink } = props;
+
+  let logoUrl = (
+    <img
+      style={{ width: "30px", opacity: 0.8 }}
+      src={logo}
+      alt="Nikhil Manohar"
+    />
+  );
+
+  return (
+    <header className={`header-area header-style-two header--fixed ${color}`}>
+      <div className="header-wrapper">
+        <div className="header-left d-flex align-items-center">
+          <div className="logo">
+            <a href={homeLink}>{logoUrl}</a>
           </div>
-          <div className="header-right">
-            <div className="social-share-inner">
-              <ul className="social-share social-style--2 color-black d-flex justify-content-start liststyle">
-                {SocialShare.map((val, i) => (
-                  <li key={i}>
-                    <a href={`${val.link}`}>{val.Social}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            {/* <div className="header-btn">
-              <a className="rn-btn" href="#contact">
-                <span>Contact me</span>
-              </a>
-            </div> */}
-            {/* Start Humberger Menu  */}
-            <div className="humberger-menu d-block d-lg-none pl--20">
-              <span
-                onClick={this.menuTrigger}
-                className="menutrigger text-white"
-              >
-                <FiMenu />
-              </span>
-            </div>
-            {/* End Humberger Menu  */}
-            <div className="close-menu d-block d-lg-none">
-              <span onClick={this.CLoseMenuTrigger} className="closeTrigger">
-                <FiX />
-              </span>
-            </div>
+          <nav className="mainmenunav d-lg-block ml--50">
+            <Scrollspy
+              className="mainmenu"
+              items={[
+                "home",
+                "about",
+                "skills",
+                "portfolio",
+                "blog",
+                "contact",
+              ]}
+              currentClassName="is-current"
+              offset={-200}
+            >
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#portfolio">Portfolio</a>
+              </li>
+              {/* <li>
+                <a href="#blog">Blog</a>
+              </li> */}
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </Scrollspy>
+          </nav>
+        </div>
+        <div className="header-right">
+          <div className="social-share-inner">
+            <ul className="social-share social-style--2 color-black d-flex justify-content-start liststyle">
+              {SocialShare.map((val, i) => (
+                <li key={i}>
+                  <a href={`${val.link}`}>{val.Social}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* <div className="header-btn">
+            <a className="rn-btn" href="#contact">
+              <span>Contact me</span>
+            </a>
+          </div> */}
+          {/* Start Humberger Menu  */}
+          <div className="humberger-menu d-block d-lg-none pl--20">
+            <span onClick={menuTrigger} className="menutrigger text-white">
+              <FiMenu />
+            </span>
+          </div>
+          {/* End Humberger Menu  */}
+          <div className="close-menu d-block d-lg-none">
+            <span onClick={closeMenuTrigger} className="closeTrigger">
+              <FiX />
+            </span>
           </div>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
+
 export default HeaderThree;
